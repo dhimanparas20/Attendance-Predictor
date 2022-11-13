3e1850cd1b2577a75b9f08fd1605468f93e39ce5
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2022-11-13 23:16:55 +0530

    code improvement
    added new features and removed shitty lines

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask_restful import Resource, Api
 from flask import render_template
-from os import system,path
+from os import system
 from flask import *
-import json,time
-import requests
 
 system("clear")
 
+# The function that predicts.
 def check(curr_lec,tot_lec):
   temp = curr_lec
   per=(curr_lec/tot_lec)*100
   perc =per
   if per >=75:
-    return curr_lec-temp,curr_lec,tot_lec,perc
+    print("\n--------| PERCENTAGE IN SAFE ZONE |--------\n")
+    no = curr_lec-temp
+    no,curr_lec,tot_lec = 0,0,0
+    return no,curr_lec,tot_lec,perc
   else:
     while per < 75 :
       curr_lec +=1
@@ -26,19 +28,21 @@ api = Api(app)
 
 class Home(Resource):
   def get(self):
-    print("--------------------------")
+    print("\n--------------------------")
     print("Home Page loaded") 
+    print("--------------------------\n")
     curr_lec = request.args.get('curr_lec')
     tot_lec = request.args.get('tot_lec')
-    print(curr_lec)
-    print(tot_lec)
-    if curr_lec  == None:
-      curr_lec,tot_lec = 1,1 
-    tmp,v1,v2,perc = check(int(curr_lec),int(tot_lec))
-    if perc >=75:
-      tmp,v1,v2 = 0,0,0
+    name = request.args.get('name')
+    print(name,curr_lec,tot_lec)
     
-    return make_response(render_template('index.html',temp=tmp,val1=v1,val2=v2,per=perc))
+    #For default page
+    if curr_lec  == None or name == None:
+      print("\n--------| None ASSIGNMNET |--------")
+      curr_lec,tot_lec,name = 1,1,""
+
+    tmp,v1,v2,perc = check(int(curr_lec),int(tot_lec))
+    return make_response(render_template('index.html',temp=tmp,val1=v1,val2=v2,per=perc,name=name))
     
 api.add_resource(Home, '/',methods=['GET', 'POST'])
 
